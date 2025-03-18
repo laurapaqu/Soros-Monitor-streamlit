5f291cb6e542fe17dfbf17ede6ac1d03a6e8ebce
laurapaqu <laura.palacio@linternaverde.co>
2025-03-18 15:00:49 -0500

    Add files via upload

--- lv_chatbot_alpha_06.py
+++ lv_chatbot_alpha_06.py
@@ -3,10 +3,11 @@ import openai
 import pandas as pd
 import os
 from dotenv import load_dotenv
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.schema import Document
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.chat_models import ChatOpenAI
+
 from langchain.chains import ConversationalRetrievalChain
 from datetime import datetime
 
